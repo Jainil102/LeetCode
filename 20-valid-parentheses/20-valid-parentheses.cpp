ce838238ac7d8e8e37bbe7732ea4d1b5dd847e47
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-02-18 23:31:21 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (50.59%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> S;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                S.push(s[i]);
+            }
+            else{
+                if(S.empty()){
+                    return false;
+                }
+                char c = S.top();
+                S.pop();
+                if(s[i] - c != 1 && s[i] - c != 2){
+                    return false;
+                }
+            }
+        }
+        if(S.empty()){
+            return true;
+        }
+        return false;
+    }
+};
