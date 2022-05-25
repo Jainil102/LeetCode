b5548cbf4e9f2a047d3d27a63233a7c5b6a3258d
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-05-25 15:57:39 +0530

    Time: 29 ms (14.82%), Space: 13.2 MB (54.43%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -17,17 +17,15 @@ public:
             }
         }
         for(int i=1; i<m; i++){
-            if(matrix[i][0] == 0){
-                for(int j=1; j<n; j++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            if(matrix[0][i] == 0){
-                for(int j=1; j<m; j++){
-                    matrix[j][i] = 0;
-                }
+        if(matrix[0][0] == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
         if(row0 == 0){
