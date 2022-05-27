bdcf93270105f742493a4e40502371803f1ee7c7
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-05-27 16:59:00 +0530

    Time: 42 ms (78.35%), Space: 19.3 MB (44.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,14 +5,13 @@ public:
         vector<vector<int>> merged;
         merged.push_back(intervals[0]);
         for(int i=1; i<intervals.size(); i++){
-            int n1 = merged.size()-1;
-            if(merged[n1][1] < intervals[i][0]){
+            if(merged.back()[1] < intervals[i][0]){
                 merged.push_back(intervals[i]);
             }
             else{
-                if(merged[n1][1] < intervals[i][1]){
+                if(merged.back()[1] < intervals[i][1]){
                     vector<int> temp;
-                    temp.push_back(merged[n1][0]);
+                    temp.push_back(merged.back()[0]);
                     temp.push_back(intervals[i][1]);
                     merged.pop_back();
                     merged.push_back(temp);
