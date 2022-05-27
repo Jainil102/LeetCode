9e111545b51f59ba8ee226a515d9a982a84d2804
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-05-27 17:00:03 +0530

    Time: 36 ms (92.83%), Space: 19.2 MB (45.25%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1){
+            return intervals;
+        }
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> merged;
         merged.push_back(intervals[0]);
