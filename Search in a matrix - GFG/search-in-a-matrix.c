0b8b539b144aa0bb320eece65a533ef3f1b4bd86
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-06-08 00:30:27 +0530

    Added solution - LeetHub

--- Search in a matrix - GFG/search-in-a-matrix.c
+++ Search in a matrix - GFG/search-in-a-matrix.c
@@ -7,26 +7,17 @@
  // } Driver Code Ends
 //User function Template for C
 
-int matSearch (int n, int m, int matrix[][m], int target){
-    if(target < matrix[0][0] || target > matrix[n-1][m-1]){
-        return 0;
-    }
+int matSearch (int n, int m, int matrix[][m], int x){
+    int r = 0;
+    int c = m-1;
     
-    for(int i=0; i<n; i++){
-        if(target >= matrix[i][0] && target <= matrix[i][m-1]){
-            int l2 = 0;
-            int h2 = m-1;
-            int m2;
-            while(l2 <= h2){
-                m2 = l2 + (h2-l2)/2;
-                if(matrix[i][m2] == target)
-                    return 1;
-                else if(matrix[i][m2] > target)
-                    h2 = m2 - 1;
-                else
-                    l2 = m2 + 1;
-            }       
-        }
+    while(r<n && c>=0){
+        if(x == matrix[r][c])
+            return 1;
+        else if(x > matrix[r][c])
+            r++;
+        else
+            c--;
     }
     
     return 0;
