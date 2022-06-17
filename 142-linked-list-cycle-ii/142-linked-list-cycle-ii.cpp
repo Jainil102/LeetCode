b4e97f971d793ed7e24bd429e45f3c2df366b230
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-06-17 23:38:54 +0530

    Time: 7 ms (90.29%), Space: 7.6 MB (33.37%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        if(head == NULL||head->next == NULL) return NULL;
+        
         ListNode* s = head;
         ListNode* f = head;
         
