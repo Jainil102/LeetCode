1fbb46b946b2f303287fef973251f75495949583
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-06-17 09:18:21 +0530

    Time: 19 ms (74.28%), Space: 11.5 MB (30.40%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -26,34 +26,23 @@ public:
         int c = n/k;
         
         while(c>0){
+            nextHead = curr;
             for(int i=1; i<=k; i++){
-                if(i==1){
-                    nextHead = curr;
-                }
                 next = curr->next;
                 curr->next = prev;
                 prev = curr;
                 curr = next;
-                if(i==k){
-                    if(c == n/k){
-                        head = prev;
-                    }
-                    else{
-                        prevHead->next = prev;
-                    }
-                }
+            }
+            if(c == n/k){
+                head = prev;
+            }
+            else{
+                prevHead->next = prev;
             }
             prevHead = nextHead;
             c--;
         }
-        
-        if(curr != NULL){
-            prevHead->next = curr;
-        }
-        else{
-            prevHead->next = NULL;
-        }
-        
+        prevHead->next = curr;
         return head;
     }
 };
