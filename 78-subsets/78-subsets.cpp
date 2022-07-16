cf78724069a0a6cd6a46ff3e654bc01414548cc9
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-07-16 14:42:35 +0530

    Time: 7 ms (34.37%), Space: 16.8 MB (8.25%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    
-    void powerset(vector<int> arr, int i, vector<int> temp){
+    void powerset(vector<int> arr, int i, vector<int> temp, vector<vector<int>> &ans){
         if(i >= arr.size()){
             ans.push_back(temp);
             return;
         }
-        powerset(arr, i+1, temp);
+        powerset(arr, i+1, temp, ans);
         temp.push_back(arr[i]);
-        powerset(arr, i+1, temp);
+        powerset(arr, i+1, temp, ans);
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
         vector<int> temp;
-        powerset(nums, 0, temp);
+        powerset(nums, 0, temp, ans);
         return ans;
     }
 };
