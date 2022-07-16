ea06874fd099ff85a38c5b38ec76ca681077cfe0
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-07-16 15:07:09 +0530

    Time: 3 ms (58.55%), Space: 5.9 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,6 +4,14 @@ public:
         if(n <= 1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i=2; i<=n; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
