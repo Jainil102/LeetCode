77116c8929c59effe984269808216317d0f6e7dd
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-07-17 22:42:53 +0530

    Time: 807 ms (5.01%), Space: 273.8 MB (5.01%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -4,23 +4,25 @@ public:
     void solve(string s, int i, string temp, vector<string> ds, vector<vector<string>> &ans){
         if(i == s.length()-1){
             temp += s[i];
-            ds.push_back(temp);
-            int flag = 0;
-            for(int j=0; j<ds.size(); j++){
-                int n = ds[j].size();
-                for(int k=0; k<n/2; k++){
-                    if(ds[j][k] != ds[j][n-k-1]){
-                        flag = 1;
-                        break;
-                    }
+            int n = temp.length();
+            for(int j=0; j<n/2; j++){
+                if(temp[j] != temp[n-j-1]){
+                    return;
                 }
-            }     
-            if(flag == 0) ans.push_back(ds);
+            }
+            ds.push_back(temp);
+            ans.push_back(ds);
             return;
         }
         
         temp += s[i];
         solve(s, i+1, temp, ds, ans);
+        int n = temp.length();
+        for(int j=0; j<n/2; j++){
+            if(temp[j] != temp[n-j-1]){
+                return;
+            }
+        }
         ds.push_back(temp);
         solve(s, i+1, "", ds, ans);
     }
