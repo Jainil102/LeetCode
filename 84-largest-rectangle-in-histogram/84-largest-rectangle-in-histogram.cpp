734cac8d674824865e74256808a56756db87d4e7
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-07-24 14:59:41 +0530

    Time: 262 ms (58.38%), Space: 77.2 MB (73.34%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -4,8 +4,8 @@ public:
         int currA, li, maxA = 0;
         stack<int> s;
         int n = heights.size();
-        for(int i=0; i<n; i++){            
-            while(!s.empty() && heights[s.top()] > heights[i]){
+        for(int i=0; i<=n; i++){            
+            while(!s.empty() && (i==n || heights[s.top()] > heights[i])){
                 int currH = heights[s.top()];
                 s.pop();
                 li = s.empty() ? -1 : s.top();
@@ -13,13 +13,6 @@ public:
                 maxA = max(maxA, currA);
             }
             s.push(i);
-        }
-        while(!s.empty()){
-            int currH = heights[s.top()];
-            s.pop();
-            li = s.empty() ? -1 : s.top();
-            currA = currH * (n - li - 1);
-            maxA = max(maxA, currA);
         }
         return maxA;
     }
