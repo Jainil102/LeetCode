ddd4068825a44141eacb503979678297737e4c09
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-08-03 22:11:01 +0530

    Time: 3 ms (76.30%), Space: 6.3 MB (57.52%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -9,18 +9,9 @@ public:
             int p = (i>=0) ? a[i--]-'0' : 0;
             int q = (j>=0) ? b[j--]-'0' : 0;
             p = p + q + carry;
-            if(p == 2){
-                carry = 1;
-                p = 0;
-            }         
-            else if(p == 3){
-                carry = 1;
-                p = 1; 
-            }
-            else{
-                carry = 0;
-            }
-            ans.insert(0, 1, p+'0');
+            q = (p&1);
+            carry = ((p>>1)&1);
+            ans.insert(0, 1, q+'0');
         }
         if(carry > 0){
             ans.insert(0, 1, carry+'0');            
