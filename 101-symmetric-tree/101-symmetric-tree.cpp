f38f1ee003a71eb7e4604c4060cfcca4a3707b3e
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-08-17 19:50:38 +0530

    Time: 12 ms (31.32%), Space: 16.9 MB (6.19%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,16 +11,34 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* left, TreeNode* right){
-        if(!left || !right){
-            return left==right;
-        }
-        if(left->val != right->val) return false;
-        
-        return solve(left->right, right->left) && solve(left->left, right->right);
-    }
-        
     bool isSymmetric(TreeNode* root) {
-        return solve(root->left, root->right);
+        if(!root->left && !root->right) return true;
+        if(!root->left || !root->right) return false;
+        stack<TreeNode*> sl;
+        stack<TreeNode*> sr;
+        sl.push(root->left);
+        sr.push(root->right);
+        while(!sl.empty() || !sr.empty()){
+            TreeNode* l = sl.top(); 
+            sl.pop();
+            TreeNode* r = sr.top(); 
+            sr.pop();
+            if(l->val != r->val) return false;
+            
+            if(l->left == NULL && r->right != NULL) return false;
+            else if(l->left != NULL && r->right == NULL) return false;
+            else if(l->left != NULL && r->right != NULL){
+                sl.push(l->left);
+                sr.push(r->right);
+            }
+            
+            if(l->right == NULL && r->left != NULL) return false;
+            else if(l->right != NULL && r->left == NULL) return false;
+            else if(l->right != NULL && r->left != NULL){
+                sl.push(l->right);
+                sr.push(r->left);
+            }
+        }
+        return true;
     }
 };
