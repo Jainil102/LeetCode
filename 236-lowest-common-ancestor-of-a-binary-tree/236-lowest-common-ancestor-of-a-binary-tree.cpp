2f53e083922189f84eff542e9564c29dec4332f0
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-08-19 19:38:32 +0530

    Time: 19 ms (79.58%), Space: 14.2 MB (85.74%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,29 +9,39 @@
  */
 class Solution {
 public:
-    bool preorder(TreeNode* node, TreeNode* target, vector<TreeNode*> &ans){  
-        if(node == NULL) return false;
-
-        ans.push_back(node);
-        if(node == target) return true;
-
-        if(preorder(node->left, target, ans) || 
-            preorder(node->right, target, ans)) return true;
-
-        ans.pop_back();
-        return false;
-    }
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> p_path;
-        preorder(root, p, p_path);
-        vector<TreeNode*> q_path;
-        preorder(root, q, q_path);
-        TreeNode* ans;
-        int i=0;
-        while(i<p_path.size() && i<q_path.size() && p_path[i] == q_path[i]){
-            ans = p_path[i++];
+    TreeNode* solve(TreeNode* node, TreeNode* p, TreeNode* q, TreeNode* &ans){
+        if(node == NULL){
+            return NULL;
         }
+        
+        TreeNode* a = solve(node->left, p, q, ans);
+        TreeNode* b = solve(node->right, p, q, ans);
+            
+        if(!a && !b){
+            if(node == p || node == q){
+                return node;
+            }    
+            return NULL;
+        }   
+        
+        if(a && b){
+            ans = node;
+            return NULL;
+        }
+        
+        if(node == p || node == q){
+            ans = node;
+            return NULL;
+        }   
+        else{
+            return node;
+        }
+        return NULL;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* ans = NULL;
+        solve(root, p, q, ans);
         return ans;
     }
 };
