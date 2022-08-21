525221f1a695773607c24c29aa920ec2253e6abb
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-08-21 22:07:02 +0530

    Time: 24 ms (40.57%), Space: 15.8 MB (82.32%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
         int a = INT_MIN;
         int ca = 0;
         int b = INT_MIN;
         int cb = 0;
+        int n = nums.size();
         for(int i=0; i<n; i++){
             if(nums[i] == a) ca++;
             else if(nums[i] == b) cb++;
             else if(ca == 0){
                 a = nums[i];
-                ca++;
+                ca = 1;
             }
-            else if(cb == 0){ 
+            else if(cb == 0){
                 b = nums[i];
-                cb++;
+                cb = 1;
             }
             else{
                 ca--;
                 cb--;
             }
         }
-        ca = 0;
-        cb = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i] == a) ca++;
-            else if(nums[i] == b) cb++;
+        ca = cb = 0;
+        for(auto i : nums){
+            if(i == a) ca++;
+            else if(i == b) cb++;
         }
         vector<int> ans;
         if(ca > n/3) ans.push_back(a);
