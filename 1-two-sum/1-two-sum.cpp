8ef9dad5b4098426d1e01aadf78e07b0b5cc1cee
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-08-28 18:25:36 +0530

    Time: 24 ms (55.55%), Space: 10.9 MB (47.58%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,15 +4,13 @@ public:
         unordered_map<int,int> m;
         vector<int> ans;
         for(int i=0; i<nums.size(); i++){
-            unordered_map<int, int>:: iterator itr = m.find(target - nums[i]);
-            if(itr != m.end()){
+            int temp = target - nums[i];
+            if(m.find(temp) != m.end()){
+                ans.push_back(m[temp]);
                 ans.push_back(i);
-                ans.push_back(itr->second);
                 break;
             }
-            else{
-                m[nums[i]] = i;
-            }
+            m[nums[i]] = i;
         }
         return ans;
     }
