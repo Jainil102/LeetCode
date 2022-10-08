1e086c8c9db0c99f63043980028b544aa36f8334
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-10-08 19:58:52 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (74.67%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int isPermuted = 0;
         int n = nums.size();
-        for(int i=n-1; i>0; i--){
-            if(nums[i] > nums[i-1]){
-                int max = INT_MAX;
-                int max_id = -1;
-                for(int j=i; j<n; j++){
-                    if(nums[j] > nums[i-1] && nums[j] <= max){
-                        max = nums[j];
-                        max_id = j;
+        int flag = 1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                flag = 0;
+                for(int j=i+1; j<=(n+i)/2; j++){
+                    swap(nums[j], nums[n+i-j]);
+                }
+                
+                for(int j=i+1; j<n; j++){
+                    if(nums[j] > nums[i]){
+                        swap(nums[j], nums[i]);
+                        break;
                     }
                 }
-                swap(nums[i-1], nums[max_id]);
-                reverse(nums, i);
-                isPermuted = 1;
                 break;
             }
         }
-        if(isPermuted == 0){
-            reverse(nums, 0);
+        if(flag){
+            for(int i=0; i<n/2; i++){
+                swap(nums[i], nums[n-1-i]);
+            }               
         }
     }
-    void reverse(vector<int>& nums, int start) {
-        int i = start, j = nums.size() - 1;
-        while (i < j) {
-            swap(nums[i], nums[j]);
-            i++;
-            j--;
-        }
-    }  
 };
