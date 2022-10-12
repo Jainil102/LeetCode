cda92aa6a84239aeefd4ad4d6935bb2ddccba828
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-10-12 09:37:10 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -14,15 +14,14 @@ class Solution{
         int ele = a[0];
         int count = 1;
         for(int i=1; i<size; i++){
+            if(count == 0){
+                ele = a[i];
+            }
             if(a[i] == ele){
                 count++;
             }
             else{
                 count--;
-            }
-            if(count == 0){
-                ele = a[i];
-                count = 1;
             }
         }
         
