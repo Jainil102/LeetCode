8fd21c833b261a3fed744975e67d5f6ca23fdeec
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-10-13 23:22:12 +0530

    Added solution - LeetHub

--- Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.cpp
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.cpp
@@ -9,8 +9,6 @@ class Solution
 {
     public:
     bool search(vector<vector<int> > matrix, int n, int m, int x) {
-        if(x<matrix[0][0] || x>matrix[n-1][m-1]) return false;
-        
         int i = 0;
         int j = m-1;
         while(i<n && j>=0){
