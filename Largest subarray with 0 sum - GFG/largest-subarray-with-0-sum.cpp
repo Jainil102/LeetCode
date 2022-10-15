b35ae1d119140fd23060fc2f05050abc774cc65c
Jainil Patel <65395432+Jainil102@users.noreply.github.com>
2022-10-15 14:00:11 +0530

    Added solution - LeetHub

---
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n){   
+        unordered_map<long,int> umap;
+        long total = 0;
+        int ans = 0;
+        for(int i=n-1; i>=0; i--){
+            total += A[i];
+            if(total == 0) ans = max(ans, n-i);
+            else {
+                if(umap.find(total) == umap.end()) umap[total] = i;
+                else {
+                    ans = max(ans, umap[total]-i);
+                }    
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
